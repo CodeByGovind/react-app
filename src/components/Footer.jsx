8d51859dd5020bbfd3cd65a11d78b425a272a7dc
CodeByGovind <govindcarpenter23@gmail.com>
2023-06-08 03:07:14 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <div>
         <h5>Follow Us</h5>
         <div>
-            <a href="https://www.youtube.com/watch?v=LITSkrAKY04&t=401s" target='blank'>Youtube</a>
+            <a href="https://www.youtube.com/watch?v=LITSkrAKY04&t=22s" target='blank'>Youtube</a>
             <a href="https://www.youtube.com/watch?v=b50zSyLiCYQ&t=2532s" target='blank'>Master React Js</a>
             <a href="https://github.com/CodeByGovind" target='blank'>Github</a>
 
