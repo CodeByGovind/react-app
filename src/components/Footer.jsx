c1ee727d4394438ef93572cc363c1d1f5a20bc5e
CodeByGovind <govindcarpenter23@gmail.com>
2023-06-08 01:57:09 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@all right resrved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.youtube.com/watch?v=LITSkrAKY04&t=401s" target='blank'>Youtube</a>
+            <a href="https://www.youtube.com/watch?v=b50zSyLiCYQ&t=2532s" target='blank'>Master React Js</a>
+            <a href="https://github.com/CodeByGovind" target='blank'>Github</a>
+
+        </div>
+
+    </div>
+</footer>
+    )
+}
+
+export default Footer
